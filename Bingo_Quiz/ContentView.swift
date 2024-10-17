3b81af727a4df7fb8e6c45fd8e73cfab427caef8
tomoya <cj.0s0ttw.nsm4@gmail.com>
2024-10-17 21:46:32 +0900

    ドラッグ&ドロップ実装

--- Bingo_Quiz/ContentView.swift
+++ Bingo_Quiz/ContentView.swift
@@ -1,24 +1,50 @@
-//
-//  ContentView.swift
-//  Bingo_Quiz
-//
-//  Created by 根岸智也 on 2024/10/16.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+  @State private var colors: [Color] = [.red, .blue, .purple, .yellow, .orange, .indigo, .cyan, .brown, .mint]
+  @State private var draggingItem: Color?
+  
+  var body: some View {
+    NavigationStack {
+      GeometryReader{ geometry in
+        let itemSize = max((geometry.size.width - 40) / 3, 0)
+        let columns = Array(repeating: GridItem(spacing: 10), count: 3)
+        LazyVGrid(columns: columns, spacing: 10, content: {
+          ForEach(colors, id: \.self) { color in
+            RoundedRectangle(cornerRadius: 10)
+              .fill(color)
+              .frame(width: itemSize, height: itemSize)
+              .draggable(color) {
+                RoundedRectangle(cornerRadius: 10)
+                  .fill(.ultraThinMaterial)
+                  .frame(width: itemSize, height: itemSize)
+                  .onAppear{
+                    draggingItem = color
+                  }
+              }
+              .dropDestination(for: Color.self) { items, location in
+                draggingItem = nil
+                return false
+              } isTargeted: { status in
+                if let draggingItem, status, draggingItem != color {
+                  if let sourceIndex = colors.firstIndex(of:draggingItem),
+                     let destinationIndex = colors.firstIndex(of: color){
+                    withAnimation(.bouncy) {
+                      let sourceItem = colors.remove(at: sourceIndex)
+                      colors.insert(sourceItem, at:destinationIndex)
+                    }
+                  }
+                }
+              }
+          }
+        })
+      }
+      .padding(20)
+      .navigationTitle("ドラッグ&ドロップ")
     }
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
